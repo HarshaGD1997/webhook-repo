179f1774b597fb9b8ceb0f0f4b56678aa4b609f7
Harsha GD <77226285+HarshaGD1997@users.noreply.github.com>
2024-06-17 21:06:32 +0530

    Delete app/config.py

--- app/config.py
+++
@@ -1,4 +0,0 @@
-
-conn_string = "mongodb+srv://harshagd:TurlnkWV065Cc5xz@cluster0.3y8bkbn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
